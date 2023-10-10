74fdd64d9ae4cd20c5c2b1f0cb415621bebc2731
LunaDheere <luna.dheere@student.ehb.be>
2023-10-10 08:32:43 +0200

    Create @1RijVanFibonacci.mjs

---
+++ @1RijVanFibonacci.mjs
@@ -0,0 +1,15 @@
+import * as readline from 'node:readline/promises';
+import{stdin as input, stdout as output} from 'node:process';
+const userInput = readline.createInterface({input, output});
+let getal = parseFloat(await userInput.question('Geef een getal in: '));
+console.log('De rij van fibonacci tot het gegeven getal: ');
+let cijfer1 = 0;
+let cijfer2 = 1;
+let rij;
+for(let i=0; i<getal; i++){
+    console.log(cijfer1);
+    rij= cijfer1+cijfer2;
+    cijfer1 = cijfer2;
+    cijfer2= rij;
+}
+process.exit();
